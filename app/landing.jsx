dc8824861320df8df4ee8c02fb59d1727fbf3a32
RamseyAndrew <anti.xe17@gmail.com>
2025-10-25 14:32:55 +0300

    added signin page

--- app/landing.jsx
+++ app/landing.jsx
@@ -10,7 +10,7 @@ export default function Landing() {
     const router = useRouter();
 
     const handleBegin = () => {
-        router.replace("/home");
+        router.push("/signin");
     };
 
     const handleBack = () => {
